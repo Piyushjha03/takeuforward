a9708e90cb3bb8fcb0b3341403c34e0e0874cf22
Piyushjha03 <jhapiyush2305@gmail.com>
2024-03-20 23:22:40 +0530

    Fix error handling in form submission

--- client/src/components/form/formPage.jsx
+++ client/src/components/form/formPage.jsx
@@ -56,9 +56,10 @@ export const FormPage = () => {
   async function handleSubmit() {
     const res = await submitData(formData);
     if( res.errors){
+      console.log(res.errors[0]);
       toast({
         variant: "destructive",
-        title: res.errors,
+        title: res.errors[0].message,
       })
     }
     else if(res.message){
